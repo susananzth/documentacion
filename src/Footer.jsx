221c0a76dee25fd5c28edd211743960a6759adc5
susananzth <susananzth.dev@gmail.com>
2023-04-21 16:58:40 -0500

    Agregando bootstrap e iconos. Mejora en nav y footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,33 +2,37 @@ import myLogo from './assets/logo.png'
 import './Footer.css'
 
 function Footer() {
-  return (
-    <div id="footer" className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <img src={myLogo} className="logo" alt="Logo de susananzth" />
-          </a>
-          <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Susana Piñero Rodríguez</span>
+    return (
+        <div id="footer" className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div className="col d-flex align-items-center">
+                    <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
+                        <img src={myLogo} className="logo" alt="Logo de susananzth" />
+                    </a>
+                    <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Susana Piñero Rodríguez</span>
+                </div>
+
+                <ul className="nav col justify-content-end">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Proyectos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Blog</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled">Contacto</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled">Curriculum CV</a>
+                    </li>
+                </ul>
+            </footer>
         </div>
-  
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-            </a>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  )
+
+    )
 }
 
 export default Footer
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,12 +3,29 @@ import './Nav.css'
 
 function Nav() {
   return (
-    <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              <img src={myLogo} className="logo" alt="Logo de susananzth" />
-                SusanaNzth
+    <nav className="container">
+        <div className="d-flex flex-wrap justify-content-center py-3 mb-4">
+            <a className="navbar-brand d-flex align-items-center mb-3 mb-md-0 me-md-auto" href="#">
+                <img src={myLogo} className="logo" alt="Logo de susananzth" />
+                Bootstrap
             </a>
+            <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Proyectos</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Blog</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled">Contacto</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled">Curriculum CV</a>
+                </li>
+            </ul>
         </div>
     </nav>
   )
