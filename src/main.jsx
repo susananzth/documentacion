6b224db4e54926b4ea0981951858f82f26798048
susananzth <susananzth.dev@gmail.com>
2023-09-30 23:17:04 -0500

    Cambio en diseño y estructura de acordeon en documentacion de javascript

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './index.css'
 import Nav from './Nav'
 import Sidebar from './Sidebar'
 import Footer from './Footer'
-import BtnCollapse from './BtnCollapse'
 
 ReactDOM.createRoot(document.getElementById('nav')).render(
   <React.StrictMode>
@@ -21,10 +20,5 @@ ReactDOM.createRoot(document.getElementById('sidebar')).render(
 ReactDOM.createRoot(document.getElementById('footer')).render(
   <React.StrictMode>
     <Footer />
-  </React.StrictMode>,
-)
-ReactDOM.createRoot(document.getElementById('btnCollapse')).render(
-  <React.StrictMode>
-    <BtnCollapse />
   </React.StrictMode>,
 )
